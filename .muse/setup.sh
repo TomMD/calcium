e9a812fac209bb178ed35900c92551f4f854845f
Thomas M. DuBuisson <tommd@musedev.io>
2020-09-08 21:45:35 -0700

    Mod

--- .muse/setup.sh
+++ .muse/setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-apt update && apt install -y libflint-dev
+apt update && apt install -y libflint-dev libflint-arb-dev
+git clone https://github.com/wbhart/antic /tmp/antic
+cd /tmp/antic
+./configure
+make
+make install
